55b474f3b676958eba444d6b871cc0cb0e6cbef5
ashu08tri <ashu500500@gmail.com>
2024-07-12 22:02:23 +0530

    updates

--- firebase.js
+++ firebase.js
@@ -7,13 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCGFQgE5ZHW-gZ9fd71tTfvAVak0tguso",
-  authDomain: "otp-ver-d8d38.firebaseapp.com",
-  projectId: "otp-ver-d8d38",
-  storageBucket: "otp-ver-d8d38.appspot.com",
-  messagingSenderId: "396859018362",
-  appId: "1:396859018362:web:ced79e3f1fcaec0f6c3fc3",
-  measurementId: "G-KYBJXEG9GQ"
+  
 };
 
 // Initialize Firebase
