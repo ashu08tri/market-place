9cc9899535071d4e5188cae37ba0f94316c04050
ashu08tri <ashu500500@gmail.com>
2024-08-14 23:53:52 +0530

    update

--- src/app/api/landingPage/banner/route.js
+++ src/app/api/landingPage/banner/route.js
@@ -10,7 +10,7 @@ if (!mongoose.connection.readyState) {
     });
   }
 
-export async function GET(request){
+export async function GET(){
     try{
         const banner = await Banner.find();
         return NextResponse.json(banner);
@@ -18,3 +18,21 @@ export async function GET(request){
         return NextResponse.json({error: err});
     }
 } 
+
+export async function POST(request){
+    const payload = {
+        image: 'https://sahara-theme.myshopify.com/cdn/shop/files/sahara-home-full-width-banner.jpg',
+        title: 'LUXURY SWIMWEAR CREATED WITH CARE',
+        title2: 'SPRING/SUMMER 2024',
+        url: '/featured/new_arrivals',
+        url2: '/featured/shop_all'
+    };
+    try{
+        const collection = new Banner(payload);
+        await collection.save();
+        return NextResponse.json({ok:true});
+    }catch(err){
+        console.log(err);
+        return NextResponse.json({status: 500})
+    }
+}
