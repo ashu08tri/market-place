3d2fa20081fdb671c4b0f71bdd53c4a6b0d97f8d
ashu08tri <ashu500500@gmail.com>
2024-08-14 23:31:05 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: 'Market Place',
+  title: 'Golden Ghaf',
   description: 'Ecommerce Website for goodies',
 };
 
